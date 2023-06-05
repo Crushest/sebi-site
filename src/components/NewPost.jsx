cea522da35ef9f42718e6b485b0d8d9bead6dd4c
Sebastian Estrada <sebi.estrada2k@gmail.com>
2023-06-05 12:42:45 -0700

    Added Modal
    Turned new post into a Modal so that it can be opened with a button and not on screen at all times.

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,20 +1,16 @@
 import classes from "./NewPost.module.css";
 
 function NewPost(props) {
-  
-
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={2} onChange={props.onBodyChange} />
+        <textarea id="body" required rows={2} onChange={props.changeBodyHandler} />
       </p>
-      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Date</label>
-        <input type="month" id="name" required onChange={props.onDateChange} />
+        <input type="text" id="name" required onChange={props.changeDateHandler} />
       </p>
-      <p>{enteredDate}</p>
     </form>
   );
 }
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -3,26 +3,32 @@ import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
+import Modal from "./Modal";
 
 function PostsList() {
-    const [enteredBody, setEnteredBody]= useState("");
-    const [enteredDate, setEnteredDate]= useState("");
- 
-  function changeBodyHandler(event) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
   }
 
-  function changeDateHandler(event) {
+  function dateChangeHandler(event) {
     setEnteredDate(event.target.value);
   }
   return (
     <>
-        <NewPost onBodyChange={changeBodyHandler} onDateChange={changeDateHandler} />
-        <ul className={classes.posts}>
+      <Modal>
+      <NewPost
+        changeBodyHandler={bodyChangeHandler}
+        changeDateHandler={dateChangeHandler}
+      />
+      </Modal>
+      <ul className={classes.posts}>
         <Post author={enteredDate} text={enteredBody} />
         <Post author="Junior" text="Apple" />
         <Post author="Tom" text="Peach" />
-        </ul>
+      </ul>
     </>
   );
 }
