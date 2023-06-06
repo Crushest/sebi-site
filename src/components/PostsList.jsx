f06fe072178a1fa983f1a7ea43840cba58a956a6
Sebastian Estrada <sebi.estrada2k@gmail.com>
2023-06-06 11:09:55 -0700

    Update PostsList.jsx

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import NewPost from "./NewPost";
 import Post from "./Post";
@@ -6,17 +6,25 @@ import classes from "./PostsList.module.css";
 import Modal from "./Modal";
 
 function PostsList({ isPosting, onStopPosting }) {
-  fetch('http://localhost:8080/posts')
   const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const responseData = await response.json();
+      setPosts(responseData.posts);
+    }
+    fetchPosts();
+  }, []);
+
   function addPostHandler(postData) {
-    fetch('http://localhost:8080/posts', {
-      method: 'POST',
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
       body: JSON.stringify(postData),
       headers: {
-        'Content-Type': 'application/json'
-      }
-  });
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
   return (
@@ -34,11 +42,11 @@ function PostsList({ isPosting, onStopPosting }) {
         </ul>
       )}
       {posts.length === 0 && (
-      <div style={{ textAlign: 'center', color: 'white' }} >
-        <h2>No Posts Found</h2>
-        <p>Start adding some!</p>
-      </div>
-    )}
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>No Posts Found</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
