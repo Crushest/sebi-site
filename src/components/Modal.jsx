8af5732c4c2f3fdabd5b0c4defb3b3405cba2ba2
Sebastian Estrada <sebi.estrada2k@gmail.com>
2023-06-19 10:58:22 -0700

    Modal Animations
    Added animations to Modal to come in and out

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 import classes from "./Modal.module.css";
 
 function Modal({ children }) {
+  const [isClosing, setIsClosing] = useState(false);
   const navigate = useNavigate();
+
   function closeHandler() {
-    navigate("..")
+    setIsClosing(true);
+    setTimeout(() => {
+      navigate("..");
+    }, 600); // Wait for the animation duration before navigating
   }
+
+  const modalClass = isClosing ? `${classes.modal} ${classes.out}` : classes.modal;
+
   return (
     <>
       <div className={classes.backdrop} onClick={closeHandler} />
-      <dialog open={true} className={classes.modal}>
+      <dialog open={true} className={modalClass}>
         {children}
       </dialog>
     </>
